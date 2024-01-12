6042059137942c5a9ae82ee684d7d7ea0daf7bbc
Shashank <kriss1897@users.noreply.github.com>
2024-01-12 22:51:12 +0530

    Show different views for sent and received invoices

--- src/components/Invoices.tsx
+++ src/components/Invoices.tsx
@@ -4,6 +4,7 @@ import {
   collection,
   getFirestore,
   onSnapshot,
+  or,
   orderBy,
   query,
   where,
@@ -12,53 +13,79 @@ import {
 import { User, getAuth } from "firebase/auth";
 import UserContext from "../context/UserContext";
 import { Button } from "@/components/ui/button";
+import { ShortAddress } from "src/utils/common";
 
-function ShortAddress({
-  address,
-  length = 3,
-}: {
-  address: string;
-  length?: number;
-}) {
-  return (
-    <p>
-      {address.slice(0, length)}...{address.slice(address.length - length)}
-    </p>
-  );
+type RawInvoice = {
+  id: string
+  from: string,
+  to: string,
+  amount: number
 }
 
 export default function Invoices() {
-  const [invoices, setInvoices] = useState<any[]>([]);
+  const [sentInvoices, setSentInvoices] = useState<any[]>([]);
+  const [receivedInvoices, setReceivedInvoices] = useState<any[]>([]);
   const { user } = useContext(UserContext);
 
   useEffect(() => {
     const db = getFirestore();
     const userId = (user as User).uid;
 
-    function formatInvoicesSnapshot(snap: QuerySnapshot) {
-      return snap.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    function formatInvoicesSnapshot(snap: QuerySnapshot): RawInvoice[] {
+      return snap.docs.map((doc) => ({ id: doc.id, ...doc.data() } as RawInvoice));
     }
 
     onSnapshot(
       query(
         collection(db, "invoices"),
-        where("from", "==", userId),
+        or(
+          where("from", "==", userId),
+          where("to", "==", userId),
+        ),
         orderBy("createdAt", "desc")
       ),
       (snap) => {
         const invoices = formatInvoicesSnapshot(snap);
+        const sent: RawInvoice[] = [];
+        const received: RawInvoice[] = [];
 
-        setInvoices(invoices);
+        invoices.forEach((invoice) => {
+          if (invoice.to === userId) {
+            received.push(invoice);
+            return;
+          }
+
+          sent.push(invoice);
+        });
+
+        setSentInvoices(sent);
+        setReceivedInvoices(received);
       }
     );
   }, []);
 
-
   return (
     <div className="space-y-4 max-w-2xl mx-auto">
-      <small className="block">Persistent Root: {treeRoot}</small>
       <Button onClick={() => getAuth().signOut()}>Signout</Button>
-      {invoices.map((invoice) => (
+      <p>Sent Invoices</p>
+      {sentInvoices.map((invoice) => (
+        <div
+          className="shadow-lg p-2 rounded-lg bg-white"
+          key={`invoice:${invoice.id}`}
+        >
+          <div className="flex flex-row">
+            <div className="grow">
+              <small className="text-gray-400 mt-4">To</small>
+              <ShortAddress address={invoice.to} length={5} />
+            </div>
+            <div className="w-32 text-center align-middle mt-8 text-xl font-medium">
+              <p>Rs. {invoice.amount}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+      <p>Received Invoices</p>
+      {receivedInvoices.map((invoice) => (
         <div
           className="shadow-lg p-2 rounded-lg bg-white"
           key={`invoice:${invoice.id}`}
@@ -67,8 +94,6 @@ export default function Invoices() {
             <div className="grow">
               <small className="text-gray-400 mt-4">From</small>
               <ShortAddress address={invoice.from} length={5} />
-              <small className="text-gray-400 mt-4">To</small>
-              <ShortAddress address={invoice.to} length={5} />
             </div>
             <div className="w-32 text-center align-middle mt-8 text-xl font-medium">
               <p>Rs. {invoice.amount}</p>
