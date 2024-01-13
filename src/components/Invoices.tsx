42340fcb7abbba4be325bd4e9e7458f4b03a1a50
Shashank <kriss1897@users.noreply.github.com>
2024-01-13 18:06:11 +0530

    Add tab component for showing sent and received invoices

--- src/components/Invoices.tsx
+++ src/components/Invoices.tsx
@@ -12,15 +12,17 @@ import {
 
 import { User, getAuth } from "firebase/auth";
 import UserContext from "../context/UserContext";
+import { ShortAddress } from "../utils/common";
+
 import { Button } from "@/components/ui/button";
-import { ShortAddress } from "src/utils/common";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 type RawInvoice = {
-  id: string
-  from: string,
-  to: string,
-  amount: number
-}
+  id: string;
+  from: string;
+  to: string;
+  amount: number;
+};
 
 export default function Invoices() {
   const [sentInvoices, setSentInvoices] = useState<any[]>([]);
@@ -32,16 +34,15 @@ export default function Invoices() {
     const userId = (user as User).uid;
 
     function formatInvoicesSnapshot(snap: QuerySnapshot): RawInvoice[] {
-      return snap.docs.map((doc) => ({ id: doc.id, ...doc.data() } as RawInvoice));
+      return snap.docs.map(
+        (doc) => ({ id: doc.id, ...doc.data() } as RawInvoice)
+      );
     }
 
     onSnapshot(
       query(
         collection(db, "invoices"),
-        or(
-          where("from", "==", userId),
-          where("to", "==", userId),
-        ),
+        or(where("from", "==", userId), where("to", "==", userId)),
         orderBy("createdAt", "desc")
       ),
       (snap) => {
@@ -67,40 +68,48 @@ export default function Invoices() {
   return (
     <div className="space-y-4 max-w-2xl mx-auto">
       <Button onClick={() => getAuth().signOut()}>Signout</Button>
-      <p>Sent Invoices</p>
-      {sentInvoices.map((invoice) => (
-        <div
-          className="shadow-lg p-2 rounded-lg bg-white"
-          key={`invoice:${invoice.id}`}
-        >
-          <div className="flex flex-row">
-            <div className="grow">
-              <small className="text-gray-400 mt-4">To</small>
-              <ShortAddress address={invoice.to} length={5} />
+      <Tabs defaultValue="account" className="w-full">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="sent">Sent</TabsTrigger>
+          <TabsTrigger value="received">Received</TabsTrigger>
+        </TabsList>
+        <TabsContent value="sent">
+          {sentInvoices.map((invoice) => (
+            <div
+              className="shadow-lg p-2 rounded-lg bg-white"
+              key={`invoice:${invoice.id}`}
+            >
+              <div className="flex flex-row">
+                <div className="grow">
+                  <small className="text-gray-400 mt-4">To</small>
+                  <ShortAddress address={invoice.to} length={5} />
+                </div>
+                <div className="w-32 text-center align-middle mt-8 text-xl font-medium">
+                  <p>Rs. {invoice.amount}</p>
+                </div>
+              </div>
             </div>
-            <div className="w-32 text-center align-middle mt-8 text-xl font-medium">
-              <p>Rs. {invoice.amount}</p>
+          ))}
+        </TabsContent>
+        <TabsContent value="received">
+          {receivedInvoices.map((invoice) => (
+            <div
+              className="shadow-lg p-2 rounded-lg bg-white"
+              key={`invoice:${invoice.id}`}
+            >
+              <div className="flex flex-row">
+                <div className="grow">
+                  <small className="text-gray-400 mt-4">From</small>
+                  <ShortAddress address={invoice.from} length={5} />
+                </div>
+                <div className="w-32 text-center align-middle mt-8 text-xl font-medium">
+                  <p>Rs. {invoice.amount}</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      ))}
-      <p>Received Invoices</p>
-      {receivedInvoices.map((invoice) => (
-        <div
-          className="shadow-lg p-2 rounded-lg bg-white"
-          key={`invoice:${invoice.id}`}
-        >
-          <div className="flex flex-row">
-            <div className="grow">
-              <small className="text-gray-400 mt-4">From</small>
-              <ShortAddress address={invoice.from} length={5} />
-            </div>
-            <div className="w-32 text-center align-middle mt-8 text-xl font-medium">
-              <p>Rs. {invoice.amount}</p>
-            </div>
-          </div>
-        </div>
-      ))}
+          ))}
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
